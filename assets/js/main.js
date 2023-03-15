ccf17c15ea250f166c9975a9b999846b80b7bbbd
usmanjchaudhry <uidman72@gmail.com>
2023-03-15 13:02:28 -0700

    Closer to perfect style

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,3 +29,18 @@ window.addEventListener('scroll', scrollHeader)
 
 
 
+function expand(card) {
+    card.classList.toggle('profile--expanded');
+
+    // If card is not expanded after toggle, add 'unexpanded' class
+    if (!card.classList.contains('profile--expanded')) card.classList.toggle('profile--unexpanded');
+    // Else if card is expanded after toggle and still contains 'unexpanded' class, remove 'unexpanded'
+    else if (card.classList.contains('profile--expanded') && card.classList.contains('profile--unexpanded')) card.classList.toggle('profile--unexpanded');
+}
+
+function toggleTheme() {
+    let docu = document.querySelector('html');
+
+    docu.classList.toggle('light-theme');
+    docu.classList.toggle('dark-theme');
+}
