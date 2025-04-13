e980ae44de23d183bd401ce6e790a346219bf9cd
Usman Chaudhry <36524989+usmanjchaudhry@users.noreply.github.com>
2025-04-13 04:58:11 -0700

    Update page.tsx

---
+++ src/app/boxingApp/page.tsx
@@ -0,0 +1,10 @@
+// src/app/boxingApp/page.tsx
+
+export default function BoxingAppPage() {
+    return (
+      <div>
+        {/* You can put your page content here later */}
+        Boxing App Page Placeholder
+      </div>
+    );
+  }
